cbd14e9b37a280aaf9a46094162d3be25ccbad22
Joao vitor <joaosoares07122004@gmail.com>
2024-10-07 17:03:01 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let totalGeral = 0;
+limpar();
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
+
+}
+
+function adicionar() {
+    //Extraindo dados do input
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorProduto = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+
+    //Calculando valor dos produtos
+    let preco = valorProduto * quantidade;
+
+    //Adicionando ao carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto"><span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span></section>`;
+
+    //Calculando valor total
+    let total = document.getElementById('valor-total');
+    totalGeral = preco + totalGeral;
+    total.textContent = `R$${totalGeral}`; 
+
+    //Resetando valor de 'quantidade'
+    document.getElementById('quantidade').value = ''; 
+        
+       
+        }
+
+
